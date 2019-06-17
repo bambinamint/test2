541befd10175a5fe5b91b5c4fe5e646952d958ce
bambinamint <51754985+bambinamint@users.noreply.github.com>
2019-06-17 09:33:12 +0700

    Initial commit

---
+++ Context/EmployeeDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test2.Context
+{
+    public class EmployeeDbContext : DbContext
+    {
+
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        DbSet<Employee> employees { get; set; }
+    }
+}
